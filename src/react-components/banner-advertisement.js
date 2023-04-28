b32af9d171fdac1815ba979ab18a79ae6a52b713
岩渕圭吾 <keingoon@gmail.com>
2023-04-28 19:54:15 +0900

    bannerのリンク追加

--- src/react-components/banner-advertisement.js
+++ src/react-components/banner-advertisement.js
@@ -6,11 +6,13 @@ export class BannerAdvertisementBox extends Component {
   render() {
     return (
       <div ref={ref => (this.root = ref)}>
-        <img
-          src="https://info.voice-doujin.space/vrbanner/banner.png"
-          className={className(styles.bannerAdvertisementBox)}
-          onError={() => (this.root.style.display = "none")}
-        />
+        <a href='https://info.voice-doujin.space/vrbanner/link.php'>
+          <img
+            src="https://info.voice-doujin.space/vrbanner/banner.png"
+            className={className(styles.bannerAdvertisementBox)}
+            onError={() => (this.root.style.display = "none")}
+          />
+        </a>
       </div>
     );
   }
